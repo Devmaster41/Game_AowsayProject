6e06464d287a0ca6769408ba40b18d4fa7a50722
Game_AwosayProject <matteaun60@gmail.com>
2020-05-23 03:11:28 +0700

    Version 3
    Add animetion  and move

---
+++ Game_AowsayProject/Assets/Playercontroler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Playercontroler : MonoBehaviour
+{
+    Vector3 target = Vector3.back;
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position,target,Speed*Time.deltaTime );
+    }
+}
