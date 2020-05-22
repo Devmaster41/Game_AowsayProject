5bc434264fdf4ebbb6148edad2b1c6ab1474ee00
Game_AwosayProject <matteaun60@gmail.com>
2020-05-23 05:59:05 +0700

    Version 5
    Add Wave

---
+++ Game_AowsayProject/Assets/WaveResponse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveResponse : MonoBehaviour
+{
+    public GameObject enemy;
+    public GameObject[] pointer;
+
+    float count = 0f;
+    public float waveTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject randomPoint = pointer[Random.Range(0, pointer.Length)];
+        count += Time.deltaTime;
+        if (count > waveTime) 
+        {
+            Instantiate(enemy, randomPoint.transform.position, Quaternion.identity);
+            count = 0f;
+        }
+    }
+}
